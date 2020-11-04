6849f375eddeb41a63e7a09e19a8dc6d389598b4
Francis Clemente <francisclemente727@yahoo.com.ph>
2020-11-04 18:11:33 +0800

    Added Initial Game

---
+++ 2D First/Assets/_Scripts/OptionInputs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionInputs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float masterVol;
+    private Text label;
+    public Slider sl;
+
+    void Start(){
+        masterVol = 0;
+    }
+
+    public void SetVol(){
+        Debug.Log(sl.value);
+        masterVol = sl.value;
+    }
+    
+    public void GetVol(){
+
+        PlayerPrefs.SetFloat("Master", sl.value);
+    }
+}
---
+++ 2D First/Assets/_Scripts/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//additional imports
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartPlay()
+    {
+        SceneManager.LoadScene(sceneName:"Options");
+    }
+
+}
