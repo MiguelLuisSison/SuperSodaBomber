7d4a85445962a0eea03249b8b1490b7b4a3cf763
Francis Clemente <francisclemente727@yahoo.com.ph>
2020-11-19 21:00:09 +0800

    Added PublicScripts for centralization

--- Super Soda Bomber/Assets/_Scripts/OptionsMenuScript.cs
+++ Super Soda Bomber/Assets/_Scripts/OptionsMenuScript.cs
@@ -5,47 +5,34 @@ using UnityEngine.UI;
 
 public class OptionsMenuScript : MonoBehaviour
 {
-    public GameObject OptionsBox;
 
     // Variable Configs
-    private Text label;
     public Slider mastersl, musicsl, sfxsl, voicesl;
-    // Start is called before the first frame update
 
     void Start()
     {
-        OptionsBox.SetActive(false);
+
+        //automatically sets the value of the slider
+        mastersl.value = PlayerPrefs.GetFloat("MasterVol", 0);
+        musicsl.value = PlayerPrefs.GetFloat("MusicVol", 0);
+        sfxsl.value = PlayerPrefs.GetFloat("SFXVol", 0);
+        voicesl.value = PlayerPrefs.GetFloat("Voice", 0);
     }
 
     // setting the vars when slider changed
-    public void SetMasterVol(){
-        Debug.Log("Master Volume: " + mastersl.value);
-        PlayerPrefs.SetFloat("MasterVol", mastersl.value);
-    }
-
-    public void SetMusicVol(){
-        Debug.Log("Music Volume: " + musicsl.value);
-        PlayerPrefs.SetFloat("MusicVol", musicsl.value);
-    }
-
-    public void SetSFXVol(){
-        Debug.Log("SFX Volume: " + sfxsl.value);
-        PlayerPrefs.SetFloat("SFXVol", sfxsl.value);
-    }
-
-    public void SetVoice(){
-        Debug.Log("Voice Toggle: " + voicesl.value);
-        PlayerPrefs.SetFloat("VoiceToggle", voicesl.value);
-    }
-
-    // Prompt Visibility
-    public void Options()
-    {
-        OptionsBox.SetActive(true);
-    }
-
-    public void ReturnOptions()
-    {
-        OptionsBox.SetActive(false);
+    public void _SetVol(Slider sl){
+        
+        if(sl == mastersl){
+            PlayerPrefs.SetFloat("MasterVol", sl.value);
+        }
+        else if(sl == musicsl){
+            PlayerPrefs.SetFloat("MusicVol", sl.value);
+        }        
+        else if(sl == sfxsl){
+            PlayerPrefs.SetFloat("SFXVol", sl.value);
+        }
+        else if (sl == voicesl){
+            PlayerPrefs.SetFloat("Voice", sl.value);
+        }
     }
 }
---
+++ Super Soda Bomber/Assets/_Scripts/PublicScripts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Contains all scripts that mostly used in the game
+//Add component at the EventSystem and then use it to the OnClick() button
+public class PublicScripts : MonoBehaviour
+{
+
+    //Moves the Scene
+    public void _Move(string scene){
+        SceneManager.LoadScene(sceneName: scene);
+    }
+    
+    //Toggles on/off the prompt
+    public void _TogglePrompt(GameObject prompt){
+        bool status = prompt.activeInHierarchy;
+        prompt.SetActive(!status);
+    }
+}
