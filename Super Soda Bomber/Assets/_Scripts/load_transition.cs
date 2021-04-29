cd0286cbea7bcb15bbb288323b4c38d88d6e1c5e
decastrojp21 <kurumiptn@gmail.com>
2021-04-29 15:39:42 +0800

    added screen transitions

---
+++ Super Soda Bomber/Assets/_Scripts/load_transition.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load_transition : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    private int levelIndex;
+    public bool fillEndAuto;
+
+
+    private void Awake()
+    {
+        if (fillEndAuto)
+            transition.SetTrigger("start_end");
+    }
+
+
+    public void LoadNextLevel(string scene)
+    {
+
+     StartCoroutine(LoadLevel(scene));
+
+
+    }
+    IEnumerator LoadLevel(string scene)
+    {
+        transition.SetTrigger("start_next0");
+
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(sceneName: scene);
+
+
+
+    }
+}
